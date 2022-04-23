276ee4e23c96c6b4bb23de52139ce968898963b5
JuJu222 <japjustin@yahoo.com>
2022-04-23 23:20:49 +0700

    Fixed padding

--- public/js/app.js
+++ public/js/app.js
@@ -64169,7 +64169,7 @@ var render = function () {
       "div",
       {
         staticClass:
-          "xl:px-20 md:px-10 sm:px-6 px-4 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center",
+          "xl:px-20 md:px-10 sm:px-6 px-2 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center",
       },
       [
         _c("div", { staticClass: "xl:w-1/3 md:w-1/2 lg:mr-16 md:mt-0" }, [
@@ -65572,7 +65572,7 @@ var render = function () {
       "div",
       {
         staticClass:
-          "xl:px-20 md:px-10 sm:px-6 px-4 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center",
+          "xl:px-20 md:px-10 sm:px-6 px-2 md:py-12 py-9 2xl:mx-auto 2xl:container md:flex items-center justify-center",
       },
       [
         _c("div", { staticClass: "xl:w-1/3 md:w-1/2 lg:mr-16 md:mt-0" }, [
